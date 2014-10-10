071bca9b7bcbc62ef25abc214362e65bf9b7be35
Manny Williams <ejw4720@g.rit.edu>
2014-10-09 21:23:40 -0400

    Added vertex matrix

---
+++ VertexMatrix.cpp
@@ -0,0 +1,8 @@
+#include "VertexMatrix.h"
+
+using namespace MidTerm;
+
+std::vector<int> VertexMatrix::GetPoints()
+{
+    return *(new std::vector<int>());
+}
---
+++ VertexMatrix.h
@@ -0,0 +1,25 @@
+#ifndef _VERTEX_MATRIX_H
+#define _VERTEX_MATRIX_H
+#include <cmatrix>
+#include <Vector>
+
+namespace MidTerm
+{
+    //Define what a matrix is
+    typedef techsoft::matrix<int> Matrix;
+
+    //Represents a point as a Homogenous Matrix
+    class VertexMatrix
+    {
+        private:
+            //Returns the X and Y coordinates after performing all transformations
+            //Index0 = X ; Index 1 = y
+            std::vector<int> GetPoints();
+
+            
+        public:
+
+    }
+}
+
+#endif
